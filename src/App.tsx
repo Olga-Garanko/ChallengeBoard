98cb56f3ae0cb2928df3a4f6b47e48543371b0ab
unknown <olga.garanko@gmail.com>
2020-12-16 01:02:13 +0200

    mongo whitelist

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ function App() {
       "role": "SHIPPER"
     })
   })
-  // fetch('/api/auth/test',
-  // {headers: {
-  //   'Content-Type': 'application/json;charset=utf-8'
-  // }})
-  // .then(result => result.json())
-  // .then(result => console.log(result));
+  fetch('/api/auth/test',
+  {headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  }})
+  .then(result => result.json())
+  .then(result => console.log(result));
   return (
     <div className="App">Hello</div>
   );
