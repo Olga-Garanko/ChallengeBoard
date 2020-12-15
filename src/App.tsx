7cdd8e8a7479129c54136496de2123bed0c45e9f
unknown <olga.garanko@gmail.com>
2020-12-15 23:55:28 +0200

    try fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import './App.css';
 
 function App() {
+  fetch('/api/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'text/json'
+    },
+    body: JSON.stringify({
+      "email": "test@test.ua",
+      "password": "123",
+      "role": "SHIPPER"
+    })
+  })
+  .then(result => console.log(result));
   return (
     <div className="App">Hello</div>
   );
